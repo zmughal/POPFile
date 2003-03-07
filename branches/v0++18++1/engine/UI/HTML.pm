fe06e65fbbca0cba5a0423fb7242cb920228a8b1
sschinke <sschinke@215d8e1a-9f62-4c3a-9943-246481b8efb3>
2003-03-07 08:32:16 +0000

    changed main history form to POST (bug 690451 and others)

--- branches/v0++18++1/engine/UI/HTML.pm
+++ branches/v0++18++1/engine/UI/HTML.pm
@@ -2845,7 +2845,7 @@ sub history_page
 
         # History page main form
 
-        $body .= "<form id=\"HistoryMainForm\" action=\"/history\" method=\"get\">\n";
+        $body .= "<form id=\"HistoryMainForm\" action=\"/history\" method=\"POST\">\n";
         $body .= "<input type=\"hidden\" name=\"search\" value=\"$self->{form}{search}\" />\n";
         $body .= "<input type=\"hidden\" name=\"sort\" value=\"$self->{form}{sort}\" />\n";
         $body .= "<input type=\"hidden\" name=\"session\" value=\"$self->{session_key}\" />\n";
