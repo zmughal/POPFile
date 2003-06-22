2a07f1c881a5dec42da8569df219a59a0a2b135f
sschinke <sschinke@215d8e1a-9f62-4c3a-9943-246481b8efb3>
2003-06-22 23:15:32 +0000

    remove some excess debug statements

--- engine/Classifier/Bayes.pm
+++ engine/Classifier/Bayes.pm
@@ -763,9 +763,7 @@ sub classify_and_modify
     my ( $self, $mail, $client, $dcount, $mcount, $nosave, $class, $echo ) = @_;
 
     $echo = 1 unless (defined $echo);
-    
-    Proxy::POP3::debug( $self,"\$dcount: $dcount, \$mcount: $mcount, \$nosave: $nosave, \$class: $class, \$echo: $echo ");
-
+        
     my $msg_subject     = '';     # The message subject
     my $msg_head_before = '';     # Store the message headers that come before Subject here
     my $msg_head_after  = '';     # Store the message headers that come after Subject here
@@ -886,9 +884,7 @@ sub classify_and_modify
     # Do the text classification and update the counter for that bucket that we just downloaded
     # an email of that type
     $classification = ($class ne '')?$class:$self->classify_file($temp_file);
-    
-    Proxy::POP3::debug( $self, "classification: $classification size: $message_size");
-
+        
     # Add the Subject line modification or the original line back again
     if ( $classification ne 'unclassified' ) {
         if ( $self->{configuration}->{configuration}{subject} ) {
