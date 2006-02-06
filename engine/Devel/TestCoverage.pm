39f1c1982353680045ff1f85ef3e89e0af91166f
texasfett <texasfett@215d8e1a-9f62-4c3a-9943-246481b8efb3>
2006-02-06 15:19:10 +0000

    Change GPL license to remove the option for the person using POPFile to license it under any version of the GPL other than v2.

--- engine/Devel/TestCoverage.pm
+++ engine/Devel/TestCoverage.pm
@@ -2,14 +2,13 @@
 #
 # Devel::TestCoverage - Module to measure code coverage in the test suite
 #
-# Copyright (c) 2001-2004 John Graham-Cumming
+# Copyright (c) 2001-2006 John Graham-Cumming
 #
 #   This file is part of POPFile
 #
-#   POPFile is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
+#   POPFile is free software; you can redistribute it and/or modify it
+#   under the terms of version 2 of the GNU General Public License as
+#   published by the Free Software Foundation.
 #
 #   POPFile is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
