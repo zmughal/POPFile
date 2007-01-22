6633015c2bb1a693bab0026bd64db64d2c73c9d8
jgrahamc <jgrahamc@215d8e1a-9f62-4c3a-9943-246481b8efb3>
2007-01-22 12:29:32 +0000

    Change to use SQLite2 by default

--- branches/b0_22_2/engine/Classifier/Bayes.pm
+++ branches/b0_22_2/engine/Classifier/Bayes.pm
@@ -217,7 +217,7 @@ sub initialize
     # $dbname can be used within it and it resolves to the full path
     # to the database named in the database parameter above.
 
-    $self->config_( 'dbconnect', 'dbi:SQLite:dbname=$dbname' );
+    $self->config_( 'dbconnect', 'dbi:SQLite2:dbname=$dbname' );
     $self->config_( 'dbuser', '' ); $self->config_( 'dbauth', '' );
 
     # SQLite 1.05+ have some problems we are resolving.  This lets us
